344e9eb86de6c0c48d69237c33b3d290dbba885a
Ashita <ashitasharma2007@gmail.com>
2026-05-27 17:34:12 +0530

    solved "Inorder traversal" problem.

---
+++ Binary Trees/inorder-traversal.py
@@ -0,0 +1,22 @@
+#Description: Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+#Approach Description: Like how we approached preorder traversal recursively, we shall apply the same concept
+#here. First we shall traverse to reach the leftmost node, add it to the list, backtrack to its parent
+#add its parent to the list and then traverse to its right sibling.
+
+#Key Concept: Inorder traversal uses Left, Root, Right traversal method
+
+
+class Solution:
+    def inorderTraversal(self, root):
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
--- Binary Trees/preorder-traversal.py
+++ Binary Trees/preorder-traversal.py
@@ -1,10 +1,11 @@
 #Description: Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
 #Approach Description: Preorder traversal is known as depth-first traversal, hence we shall
-#start by traversing until we reach the leftmost node, append in to the list,
+#start by traversing until we reach the leftmost node, append its parent to the list, add it to the list,
 #go up one level to its parent, go to its right child, add it to the list and recursively
 #apply this method until we reach have traversed each element once.
 
+#Key Concept; Inorder traversal uses Root, Left, Right traversal method.
 class Solution:
     def preorderTraversal(self, root):
         res = []
