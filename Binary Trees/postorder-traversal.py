bcc95cd1fc1cdc2e3f36b6f73417e98fd5d9cb17
Ashita <ashitasharma2007@gmail.com>
2026-05-27 17:43:08 +0530

    solved "Postorder traversal" problem.

---
+++ Binary Trees/postorder-traversal.py
@@ -0,0 +1,23 @@
+#Description: Given the root of a binary tree, return the postorder traversal of its nodes' values
+
+#Approach Description: Similar to the previous two questions, we shall recursively reach the leftmost node,
+#append the left node, move to the right node to append it, then append the parent node.
+#recursively apply the same logic until all elements are traversed
+
+#Key Concept: Postorder traversal uses Left, Right, Root traversal method.
+
+class Solution:
+    def postorderTraversal(self, root):
+        res = []
+
+        def postorder(root):
+            if not root:
+                return
+
+            postorder(root.left)
+            postorder(root.right)
+            res.append(root.val)
+
+        postorder(root)
+        return res
+
