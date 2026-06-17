c46f8913b8ae68c429959024ba78fdac2ed76846
Ashita <ashitasharma2007@gmail.com>
2026-06-17 15:56:50 +0530

    solved "reduction operations on array" problem

--- Array/1887-reduction-operations-on-array.py
+++ Array/1887-reduction-operations-on-array.py
@@ -1,9 +1,9 @@
-# Description: Given an integer array nums, your goal is to make all elements in nums equal.
-# To complete one operation, follow these steps:
-# Find the largest value in nums. Let its index be i (0-indexed) and its value be largest. If there are multiple elements with the largest value, pick the smallest i.
-# Find the next largest value in nums strictly smaller than largest. Let its value be nextLargest.
-# Reduce nums[i] to nextLargest.
-# Return the number of operations to make all elements in nums equal.
+#Description: Given an integer array nums, your goal is to make all elements in nums equal.
+#To complete one operation, follow these steps:
+#Find the largest value in nums. Let its index be i (0-indexed) and its value be largest. If there are multiple elements with the largest value, pick the smallest i.
+#Find the next largest value in nums strictly smaller than largest. Let its value be nextLargest.
+#Reduce nums[i] to nextLargest.
+#Return the number of operations to make all elements in nums equal.
 
 #Approach: In a reverse sorted array, for each new element we see, we know that we have to convert all
 #previous numbers to the current number. Hence the count of operations increases by 1.
